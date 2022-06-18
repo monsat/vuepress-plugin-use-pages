c50060f1cbd929ab740d3468cc9848ba10ac189f
mon_sat <tnk@daijiten.info>
2022-06-18 17:13:18 +0900

    refactor: import plugin file

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,46 +1,5 @@
-import type { Page, PluginObject } from '@vuepress/core'
+import { usePagesPlugin } from './usePagesPlugin'
 
-export interface UsePagesPluginOptions {
-  startsWith?: string
-  filter?: (page: Page) => boolean
-  sort?: (a: Page, b: Page) => number
-  limit?: number | false
-  file?: string
-}
-
-export const usePagesPlugin = (options: UsePagesPluginOptions): PluginObject => {
-  const name = 'vuepress-plugin-use-pages'
-  const multiple = true
-
-  const onPrepared: PluginObject['onPrepared'] = (app) => {
-    const defaultSort = (a: Page, b: Page) => {
-      if (!a.data.frontmatter.date || !b.data.frontmatter.date) {
-        return 0
-      }
-      return (new Date(b.data.frontmatter.date).getTime()) - (new Date(a.data.frontmatter.date).getTime())
-    }
-    const {
-      startsWith = '/articles/',
-      filter,
-      sort = defaultSort,
-      limit = false,
-      file = 'pages.js',
-    }: UsePagesPluginOptions = options || {}
-
-    const docs = app.pages.filter(p => p.data.path.startsWith(startsWith))
-    const filtered = filter ? docs.filter(filter) : docs
-    filtered.sort(sort)   // Sorted in place
-    const limited = limit ? filtered.slice(0, limit) : filtered
-    const pageData = limited.map(p => p.data)
-    const content = `export const usePages = () => ${JSON.stringify(pageData)}`
-    app.writeTemp(file, content)
-  }
-
-  return {
-    name,
-    multiple,
-    onPrepared,
-  }
-}
+export * from './usePagesPlugin'
 
 export default usePagesPlugin
